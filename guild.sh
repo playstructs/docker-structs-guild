228fc75915b4418f0c19d159b63ad18ed03f4dba
abstrct <abstrct@slow.ninja>
2024-12-11 00:32:08 -0500

    hackin together a utility script

---
+++ guild.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+echo "Structs Guild Stack"
+
+# Does the webapp source exist anywhere?
+# If no:
+  # clone it to a tmp folder somewhere
+# If yes:
+  # Request a folder
+    # Check the contents to see if it roughly looks like webapp should
+
+
+
+# launch php tests
+
